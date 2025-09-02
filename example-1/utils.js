3fff80090f404f5227fae5abfb5014774237c481
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-09-02 23:17:16 +0300

    draw touch

--- example-1/utils.js
+++ example-1/utils.js
@@ -28,3 +28,25 @@ utils.captureMouse = function (element) {
   );
   return mouse;
 };
+
+utils.drawCircle = (
+  ctx,
+  x,
+  y,
+  radius = 30,
+  fillColor = "#a2b9eb",
+  strokeColor = null,
+  lineWidth = 1
+) => {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  if (fillColor) {
+    ctx.fillStyle = fillColor;
+    ctx.fill();
+  }
+  if (strokeColor) {
+    ctx.strokeStyle = strokeColor;
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
+  }
+}
